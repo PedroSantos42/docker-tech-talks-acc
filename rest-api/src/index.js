883fce47ab9e4b7737d025b0d2c13f1c3e2ee313
pedro.santos <pedroabs.dev42@gmail.com>
2020-05-20 23:50:54 -0300

    mongodb

--- rest-api/src/index.js
+++ rest-api/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoose = require('mongoose')
 
 class App {
     constructor() {
         this.express = express()
         this.middlewares()
         this.routes()
+        this.database();
     }
 
     middlewares() {
@@ -14,6 +16,13 @@ class App {
 
     routes() {
         this.express.use(routes)
+    }
+
+    database() {
+        mongoose.connect('mongodb://tt-mongodb:27017/tech-talks-database', {
+            keepAlive: 1, useUnifiedTopology: true, useNewUrlParser: true
+        }, (err) => err ? console.log(err) : console.log('Conectado ao MongoDB...'));
+        mongoose.set('useCreateIndex', true)
     }
 }
 
